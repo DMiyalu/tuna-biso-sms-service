0f49d6c41d2a4c56448017249324397f9e84bb91
DMiyalu <dieudonnemiyalu6@gmail.com>
2021-10-30 08:37:36 +0100

    quick fix

--- sms/sms_router.js
+++ sms/sms_router.js
@@ -1,18 +1,22 @@
 const { request, response } = require('express')
 const express = require('express')
 const router = express.Router()
+const { sendCustomSms, sendDiffusion } = require('./sms_controller')
 
-router.get('send/', (request, response) => {
-    response.send('send sms');
+router.post('/send/', (request, response) => {
+    const {phone, content} = request.body
+    sendCustomSms(phone, content)
+    response.send(`send sms: ${phone}: ${content}`)
 })
 
-router.get('diffusion/', (request, response) => {
-    response.send('sms diffusion');
+router.post('/diffusion/', (request, response) => {
+    const {phoneList, content} = request.body
+    sendDiffusion(phoneList, content)
+    response.send(`send diffusion:${phoneList.length}.${phoneList}: ${content}`)
 })
 
-router.post('webhooks/inbound', (request, response) => {
-    // response.status(200).end();
-    response.send('webhooks');
+router.post('/webhooks/inbound', (request, response) => {
+    response.status(200).end();
 })
 
 module.exports = router
